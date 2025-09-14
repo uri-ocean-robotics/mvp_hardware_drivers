adcba93ff488fdec89b6b17ae99cb891cda264c5
mzhouURI <mzhou@uri.edu>
2025-09-14 14:21:42 -0400

    added dropweight strobe script

---
+++ mvp_gpio_manager/src/drop_weight.py
@@ -0,0 +1,31 @@
+#  Blink an LED with the LGPIO library
+#  Uses lgpio library, compatible with kernel 5.11
+#  Author: William 'jawn-smith' Wilson
+
+import time
+import lgpio
+
+DROPWEIGHT = 11
+STROBE = 12
+
+# open the gpio chip and set the LED pin as output
+h = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_output(h, DROPWEIGHT)
+lgpio.gpio_claim_output(h, STROBE)
+
+try:
+    while True:
+        # Turn the GPIO pin on
+        lgpio.gpio_write(h, DROPWEIGHT, 1)
+        lgpio.gpio_write(h, STROBE, 1)
+        time.sleep(1)
+
+        # Turn the GPIO pin off
+        lgpio.gpio_write(h, DROPWEIGHT, 0)
+        lgpio.gpio_write(h, STROBE, 0)
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    lgpio.gpio_write(h, DROPWEIGHT, 0)
+    lgpio.gpio_write(h, STROBE, 0)
+    lgpio.gpiochip_close(h)
